7950f3533969cfb8d757b23adc740efbf3f1210c
pltduchoang <plt.duchoang@gmail.com>
2023-09-21 17:15:48 -0600

    Week3-1

--- app/Week3/page.js
+++ app/Week3/page.js
@@ -3,9 +3,11 @@ import ItemList from "./item-list";
 
 export default function Page() {
     return (
-    <main className="flex flex-col items-center">
-        <Link href="/">Home</Link>
-        <h1>My Shopping List</h1>
+    <main className="flex flex-col items-center w-full bg-emerald-100">
+        <div className="bg-emerald-600 text-white p-4 my-4 rounded-lg text-center w-60 hover:bg-emerald-400 hover:underline">
+          <Link href="/">Home</Link>
+        </div>
+        <h1 className=" bg-green-500 text-white flex justify-center p-4 m-4 rounded-md border-black-600 w-60 items-center">My Shopping List</h1>
         <br/>
         <ItemList/>
     </main>
--- app/page.js
+++ app/page.js
@@ -4,19 +4,19 @@ import StudentInfo from './StudentInfo/StudentInfo';
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen p-24 items-center bg-green-800">
+    <main className="flex flex-col min-h-screen p-24 items-center bg-emerald-100">
       <div>
         <div>
-          <h1 className="text-center text-white bg-emerald-400 rounded-md p-4">CPRG-306 assignments</h1>
+          <h1 className="text-center text-green-500 bg-green-300 rounded-md p-4">CPRG-306 assignments</h1>
         </div>
         <br/>
-        <div className="bg-slate-400 rounded-xl p-8">
+        <div className="bg-green-500 rounded-xl p-8 text-white">
           <StudentInfo/>
         </div>
-        <div className="bg-slate-100 p-4 my-4 rounded-lg text-center">
+        <div className="bg-emerald-600 text-white p-4 my-4 rounded-lg text-center hover:bg-emerald-400 hover:underline">
           <Link href="/Week2">To Week 2 Page</Link>
         </div>
-        <div className="bg-slate-100 p-4 my-4 rounded-lg text-center">
+        <div className="bg-emerald-600 text-white p-4 my-4 rounded-lg text-center hover:bg-emerald-400 hover:underline">
           <Link href="/Week3">To Week 3 Page</Link>
         </div>
       </div>
