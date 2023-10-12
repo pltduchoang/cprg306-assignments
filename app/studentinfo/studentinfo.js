668f3d8dd5a52c6c66da4a79fc3ac22144033aa4
pltduchoang <plt.duchoang@gmail.com>
2023-10-12 15:00:26 -0600

    Create studentinfo.js

---
+++ app/studentinfo/studentinfo.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function StudentInfo() {
+    return (
+        <main className="bg-green-500 rounded-xl p-8 text-white">
+            <h1>Student Info</h1>
+            <p>Name: Anh Duc Hoang / Duc Hoang</p>
+            <p>Course Section: CPRG 306 B</p>
+            <Link href="https://github.com/pltduchoang/cprg306-assignments.git">My Github</Link>
+        </main>)
+}
