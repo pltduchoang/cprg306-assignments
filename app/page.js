172dd809d2e266b7e795a42f64e12078b872ace1
pltduchoang <plt.duchoang@gmail.com>
2023-10-19 15:03:16 -0600

    update

--- app/page.js
+++ app/page.js
@@ -48,5 +48,5 @@ export default function Home() {
       </div>
       
     </main>
-  )
+  );
 }
--- app/week3/page.js
+++ app/week3/page.js
@@ -9,5 +9,5 @@ export default function Page() {
         <h1 className="text-2xl text-green-500 bg-green-300 flex justify-center p-4 m-4 rounded-md border-black-600 w-60 items-center">My Shopping List</h1>
         <ItemList/>
     </main>
-    )
+    );
 }
